0b6a91a4c8654ca03ddbcda362d25e75a99eaebb
SasankAtTechStar <saisasank.v@techstargroup.com>
2026-06-29 13:03:48 +0530

    Change text display to JSON output for response

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,4 @@ if ingredient_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+st.write(smoothiefroot_response.json())
