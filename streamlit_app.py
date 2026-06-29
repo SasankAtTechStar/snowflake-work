2a2e92e32ad47bedba59639f39f116f208a3f50a
SasankAtTechStar <saisasank.v@techstargroup.com>
2026-06-29 14:15:19 +0530

    Convert Snowflake DataFrame to Pandas DataFrame
    Convert Snowflake DataFrame to Pandas DataFrame for further processing.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ st.write('The name on your Smoothie will be:',name_on_order)
 
 session = st.connection("snowflake").session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 # st.dataframe(data=my_dataframe, width='stretch')
 # st.stop()
 
