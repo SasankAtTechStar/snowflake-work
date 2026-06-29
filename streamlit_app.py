9444778be26f258a20de8b30fac731a208ca2d20
SasankAtTechStar <saisasank.v@techstargroup.com>
2026-06-29 14:05:25 +0530

    Include SEARCH_ON in fruit options data frame
    Added SEARCH_ON column to the fruit options data frame and displayed it in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:',name_on_order)
 
 session = st.connection("snowflake").session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, width='stretch')
+st.stop()
 ingredient_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
